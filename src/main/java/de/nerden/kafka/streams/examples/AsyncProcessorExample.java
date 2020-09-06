cfaa34723984f713073814cfa554a4e69578f56f
Johannes Brüderl <johannes.bruederl@gmail.com>
2020-09-06 03:58:40 +0200

    cleanup, fix imports

--- src/main/java/de/nerden/kafka/streams/examples/AsyncProcessorExample.java
+++ src/main/java/de/nerden/kafka/streams/examples/AsyncProcessorExample.java
@@ -1,18 +1,11 @@
 package de.nerden.kafka.streams.examples;
 
-import de.nerden.kafka.streams.processor.AsyncProcessor;
 import de.nerden.kafka.streams.processor.AsyncProcessorSupplier;
-import de.nerden.kafka.streams.serde.KeyValueSerde;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.errors.LogAndFailExceptionHandler;
-import org.apache.kafka.streams.state.KeyValueStore;
-import org.apache.kafka.streams.state.StoreBuilder;
-import org.apache.kafka.streams.state.Stores;
 
 import java.util.Properties;
 
@@ -27,8 +20,8 @@ public class AsyncProcessorExample {
     t.addSource("data", Serdes.String().deserializer(), Serdes.String().deserializer(), "data");
     t.addProcessor(
         "async",
-            new AsyncProcessorSupplier<>(
-                    "data-backlog", Serdes.String(), Serdes.String(), System.out::println),
+        new AsyncProcessorSupplier<>(
+            "data-backlog", Serdes.String(), Serdes.String(), System.out::println),
         "data");
 
     KafkaStreams streams = new KafkaStreams(t, getProperties());
@@ -37,13 +30,8 @@ public class AsyncProcessorExample {
 
   private static Properties getProperties() {
     Properties props = new Properties();
-    props.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
-    props.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
-    props.setProperty(StreamsConfig.REPLICATION_FACTOR_CONFIG, "1");
-    props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-    props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, "10000");
-    props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 100 * 1024 * 1024L); // 100 MB
-    props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, 1);
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
     props.put(
         StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
         LogAndFailExceptionHandler.class.getName());
