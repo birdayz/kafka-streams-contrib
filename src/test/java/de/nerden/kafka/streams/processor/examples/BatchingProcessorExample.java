6840425c3b3d0dfa5eab1a189f4a67d4edd33928
Johannes Brüderl <johannes.bruederl@gmail.com>
2020-11-17 23:38:57 +0100

    add example for BatchTransformer

---
+++ src/test/java/de/nerden/kafka/streams/processor/examples/BatchingProcessorExample.java
@@ -0,0 +1,51 @@
+package de.nerden.kafka.streams.processor.examples;
+
+import de.nerden.kafka.streams.MoreTransformers;
+import java.util.Properties;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.utils.Bytes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.errors.LogAndFailExceptionHandler;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueStore;
+
+public class BatchingProcessorExample {
+
+  private static final String KAFKA_BROKERS = "localhost:9092";
+  private static final String APPLICATION_ID = "testBatch";
+
+  public static void main(String[] args) {
+    StreamsBuilder builder = new StreamsBuilder();
+    KStream<String, String> data =
+        builder.stream("data", Consumed.with(Serdes.String(), Serdes.String()));
+
+    data.transform(
+            MoreTransformers.Batch(
+                Materialized.<String, String, KeyValueStore<Bytes, byte[]>>as("batch")
+                    .withKeySerde(Serdes.String())
+                    .withValueSerde(Serdes.String())))
+        .peek(
+            (key, value) -> {
+              System.out.println(value);
+            });
+
+    KafkaStreams streams = new KafkaStreams(builder.build(), getProperties());
+    streams.start();
+  }
+
+  private static Properties getProperties() {
+    Properties props = new Properties();
+    props.put("request.timeout.ms", 60000);
+    props.put(StreamsConfig.APPLICATION_ID_CONFIG, APPLICATION_ID);
+    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
+    props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 0L);
+    props.put(
+        StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG,
+        LogAndFailExceptionHandler.class.getName());
+    return props;
+  }
+}
