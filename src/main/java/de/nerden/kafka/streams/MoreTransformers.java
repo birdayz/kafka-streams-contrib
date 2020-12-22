3c4b91770cd15b3331564cf2fb9a578d5affba23
Johannes Brüderl <johannes.bruederl@gmail.com>
2020-12-22 23:36:30 +0100

    improve javadoc

--- src/main/java/de/nerden/kafka/streams/MoreTransformers.java
+++ src/main/java/de/nerden/kafka/streams/MoreTransformers.java
@@ -1,7 +1,6 @@
 package de.nerden.kafka.streams;
 
 import de.nerden.kafka.streams.processor.BatchTransformerSupplier;
-import java.util.List;
 import org.apache.kafka.common.utils.Bytes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Materialized;
@@ -9,17 +8,24 @@ import org.apache.kafka.streams.kstream.TransformerSupplier;
 import org.apache.kafka.streams.kstream.internals.MaterializedInternal;
 import org.apache.kafka.streams.state.KeyValueStore;
 
+import java.util.List;
+
 public class MoreTransformers {
+
+  public static final long DEFAULT_MAX_BATCH_DURATION_MILLIS = 10000L;
+  public static final long DEFAULT_MAX_BATCH_SIZE_PER_KEY = 1000L;
 
   public static <K, V> TransformerSupplier<K, V, KeyValue<K, List<V>>> Batch(
       Materialized<K, V, KeyValueStore<Bytes, byte[]>> materialized) {
-    return Batch(materialized, 10000L, 1000L);
+    return Batch(materialized, DEFAULT_MAX_BATCH_DURATION_MILLIS, DEFAULT_MAX_BATCH_SIZE_PER_KEY);
   }
 
   /**
-   * @param materialized Materialization information
-   * @param maxBatchDurationMillis This is the upper bounds in terms of time, how long a batch will
-   *     be waited for.
+   * @param materialized Materialization information for the state stored used for batching.
+   * @param maxBatchDurationMillis Every time maxBatchDurationMillis passed, batches are released
+   *     for all keys.
+   * @param maxBatchSizePerKey When matchBatchSizePerKey records have been collected for a specific
+   *     key, the batch is forwarded be waited for.
    * @return
    */
   public static <K, V> TransformerSupplier<K, V, KeyValue<K, List<V>>> Batch(
